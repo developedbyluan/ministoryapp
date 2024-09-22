40727fed6025777dd509691993373c09181d9076
Luan Tran <developedbyluan@gmail.com>
2024-09-22 18:40:13 +0700

    Create layout, home page

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next"
+import React from "react"
+export const metadata: Metadata = {
+    title: "Mini Story App",
+    description: "This app is a great supplement for low beginning learners who want to learn English/Chinese/Spanish/Arabic deeply to increase vocabulary, comprehension with step by step transcriptions and AI assistant.",
+    keywords: ["effortless", "english", "mini", "story", "story", "learning", "aj hoge", "vietnam", "vietnamese", "chinese", "spanish", "arabic", "deep", "learning", "listen", "answer", "tpr", "tprs", "tprstorytelling"],
+}
+export default function RootLayout(props: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>
+                { props.children }
+            </body>
+        </html>
+    )
+}
