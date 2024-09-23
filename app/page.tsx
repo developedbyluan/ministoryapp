1508af318f5e6495ebc42cad9798a8abd7d5ae35
Luan Tran <developedbyluan@gmail.com>
2024-09-23 16:58:33 +0700

    Create functionality: Language Selector

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,48 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 export default function HomePage() {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    )
+  const router = useRouter();
+  const STORAGE_KEY = "preferredLanguage";
+  const languages = [
+    { id: "english", name: "English", route: "/english" },
+    { id: "chinese", name: "Chinese", route: "/chinese" },
+    { id: "spanish", name: "Spanish", route: "/spanish" },
+    { id: "arabic", name: "Arabic", route: "/arabic" },
+  ];
+
+  React.useEffect(() => {
+    const preferredLanguage = localStorage.getItem(STORAGE_KEY);
+
+    if (!preferredLanguage) return;
+
+    if (preferredLanguage) {
+      const language = languages.find((lang) => lang.id === preferredLanguage);
+      if (language) {
+        router.push(language.route);
+      }
+    }
+  }, []);
+  return (
+    <div>
+      <h1>Choose Language</h1>
+      <ul>
+        <li>
+          <Link href="/english">English</Link>
+        </li>
+        <li>
+          <button>Chinese</button>
+        </li>
+        <li>
+          <button>Spanish</button>
+        </li>
+        <li>
+          <button>Arabic</button>
+        </li>
+      </ul>
+    </div>
+  );
 }
