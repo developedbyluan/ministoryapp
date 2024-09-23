414b1f9bf966b10a438627a3f2f749ba3bf0b3f3
Luan Tran <developedbyluan@gmail.com>
2024-09-23 17:10:37 +0700

    Set preferred language to English in local storage on English page load

--- app/english/page.tsx
+++ app/english/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import React from "react";
 import Link from "next/link";
 
 export default function EnglishPage() {
+    React.useEffect(() => {
+        localStorage.setItem("preferredLanguage", "english");
+    }, []);
     return (
         <div>
             <h1>English</h1>
