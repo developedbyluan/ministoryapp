eb6d84fd6742b11cb5987c85b09618133dbaa299
Luan Tran <developedbyluan@gmail.com>
2024-09-24 15:37:18 +0700

    Revert "Import MP3Provider to layout of english"
    This reverts commit fe2d44305a66440d870eab9de47c6e965e7493ee.

--- app/english/[...lesson]/layout.tsx
+++ app/english/[...lesson]/layout.tsx
@@ -1,10 +1,5 @@
-import { MP3Provider } from "@/app/english/contexts/MP3Context";
 export default function EnglishLessonLayout(props: {
   children: React.ReactNode;
 }) {
-  return (
-    <MP3Provider>
-      <div>{props.children}</div>
-    </MP3Provider>
-  );
+  return <div>{props.children}</div>;
 }
