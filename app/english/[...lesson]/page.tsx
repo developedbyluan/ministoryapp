d91331a924a3c74097a076027a761fd710a9a55c
Luan Tran <developedbyluan@gmail.com>
2024-09-24 15:46:10 +0700

    Integrate MP3 context in EnglishLessonPage
    - Import useMP3 hook from MP3Context
    - Retrieve mp3file from context using useMP3 hook
    - Add console log to display mp3file for debugging

--- app/english/[...lesson]/page.tsx
+++ app/english/[...lesson]/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { useMP3 } from "@/app/english/contexts/MP3Context";
 
 export default function EnglishLessonPage() {
   const params = useParams();
   const lessonName = params.lesson[1];
   const lessonData = JSON.parse(localStorage.getItem(lessonName) || "{\"error\": \"Lesson not found in local storage\"}");
+  const { mp3file } = useMP3();
   console.log(lessonData);
+  console.log(mp3file);
   if (lessonData.error) {
     return <div>Error: {lessonData.error}</div>;
   }
