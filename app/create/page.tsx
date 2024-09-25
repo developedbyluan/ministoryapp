5fd97f3876d7ea861f13e4311eec1a87d50e073f
Luan Tran <developedbyluan@gmail.com>
2024-09-25 08:18:39 +0700

    Handle file upload

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function TranscriptionEditorPage() {
+  function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+    if (file) {
+      console.log(file);
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const content = e.target?.result as string;
+        console.log(content);
+      };
+
+      reader.readAsText(file);
+    }
+  }
   return (
     <div className="flex flex-col gap-7 items-center p-4">
-      <Input type="file" accept=".txt" />
+      <Input type="file" accept=".txt" onChange={handleFileUpload} />
       <div className="flex flex-col gap-7 items-center py-7">
         <div className="flex flex-col items-center">
           <p>
@@ -18,7 +34,8 @@ export default function TranscriptionEditorPage() {
             It was 5:00 and Alan was riding his motorcycle in San Francisco.
           </p>
         </div>
-      </div> {/* ./ log line ui */}
+      </div>{" "}
+      {/* ./ log line ui */}
     </div>
   );
 }
