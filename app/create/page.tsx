198c1ea86d9a2effd9357fab1f13191f53e2e7ab
Luan Tran <developedbyluan@gmail.com>
2024-09-25 08:02:26 +0700

    Create UI: log line

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button"
+
 export default function TranscriptionEditorPage() {
-  return <div>Transcription Editor</div>;
+return (
+  <div className="flex flex-col gap-7 items-center py-7">
+    <div className="flex flex-col items-center">
+      <p>Hello, welcome to the mini-story for The Race. Let’s get started.</p>
+      <Button>Log</Button>
+    </div>
+
+    <div className="flex flex-col items-center">
+      <p>It was 5:00 and Alan was riding his motorcycle in San Francisco.</p>
+    </div>
+  </div>
+)
 }
