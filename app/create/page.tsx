9ce7356ae84b21de211111009e040dd7cde9f6db
Luan Tran <developedbyluan@gmail.com>
2024-09-25 08:09:26 +0700

    Create UI: upload text file

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,16 +1,24 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function TranscriptionEditorPage() {
-return (
-  <div className="flex flex-col gap-7 items-center py-7">
-    <div className="flex flex-col items-center">
-      <p>Hello, welcome to the mini-story for The Race. Let’s get started.</p>
-      <Button>Log</Button>
-    </div>
+  return (
+    <div className="flex flex-col gap-7 items-center p-4">
+      <Input type="file" accept=".txt" />
+      <div className="flex flex-col gap-7 items-center py-7">
+        <div className="flex flex-col items-center">
+          <p>
+            Hello, welcome to the mini-story for The Race. Let’s get started.
+          </p>
+          <Button>Log</Button>
+        </div>
 
-    <div className="flex flex-col items-center">
-      <p>It was 5:00 and Alan was riding his motorcycle in San Francisco.</p>
+        <div className="flex flex-col items-center">
+          <p>
+            It was 5:00 and Alan was riding his motorcycle in San Francisco.
+          </p>
+        </div>
+      </div> {/* ./ log line ui */}
     </div>
-  </div>
-)
+  );
 }
