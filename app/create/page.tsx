f126cb53c3baf407a80284fd21ef0c76917271e7
Luan Tran <developedbyluan@gmail.com>
2024-09-25 09:32:36 +0700

    Add text to transcriptions[]

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -2,11 +2,12 @@
 
 import React from "react";
 
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function TranscriptionEditorPage() {
   const [blocks, setBlocks] = React.useState<string[]>([]);
+  const [transcriptions, setTranscriptions] = React.useState<string[]>([]);
 
   function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
     const file = e.target.files?.[0];
@@ -22,13 +23,25 @@ export default function TranscriptionEditorPage() {
       reader.readAsText(file);
     }
   }
+
+  function logLine(index: number) {
+    const lines = blocks[index].split("\n");
+    setTranscriptions((prev) => [...prev, lines[0]]);
+  }
+
   return (
     <div className="flex flex-col gap-7 items-center p-4">
       <Input type="file" accept=".txt" onChange={handleFileUpload} />
       <div className="flex flex-col gap-7 items-start py-7">
+        {transcriptions.map((transcription, index) => (
+          <p key={crypto.randomUUID()}>{transcription}</p>
+        ))}
+      </div>
+      <div className="flex flex-col gap-7 items-start py-7">
         {blocks.map((block, index) => (
-          <div key={index} className="flex items-center">
+          <div key={crypto.randomUUID()} className="flex items-center">
             <p>{block}</p>
+            <Button onClick={() => logLine(index)}>Log</Button>
           </div>
         ))}
       </div>
